95f3f2490367b1560f06d9c0d00db3bac39a732e
khuephamy <khuephumy19@gmail.com>
2023-05-13 14:34:45 -0500

    nearby job section

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -7,7 +7,7 @@ import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress}>
+    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress(item)}>
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
           source={{
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -17,7 +17,7 @@ const NearbyJobs = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <Text style={styles.headerTitle}>Nearby jobs</Text>
         <TouchableOpacity>
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
@@ -29,13 +29,13 @@ const NearbyJobs = () => {
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
-          <FlatList
-            data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
-            keyExtractor={(item) => item?.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
-          />
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`job-details/${job.job_id}`)}
+            />
+          ))
         )}
       </View>
     </View>
