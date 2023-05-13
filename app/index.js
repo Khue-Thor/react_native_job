b35fe67a1c454b021797168d2e28fc97fffa1919
khuephamy <khuephumy19@gmail.com>
2023-05-13 11:47:36 -0500

    init

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { exp } from "react-native-reanimated";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
