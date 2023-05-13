649cfd99b6c9c9afe924d12d1206c3dacae276a2
khuephamy <khuephumy19@gmail.com>
2023-05-13 12:13:21 -0500

    imported components

--- app/index.js
+++ app/index.js
@@ -1,12 +1,38 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 import { exp } from "react-native-reanimated";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome, welcome } from "../components";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />,
 
-export default Home
+          headerRight: () => <ScreenHeaderBtn iconUrl={icons.profile} dimension="60%" />,
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
