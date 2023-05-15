2558c65985a0bd35f1023ccaa03d2d2994f3975f
khuephamy <khuephumy19@gmail.com>
2023-05-14 19:13:17 -0500

    updated

---
+++ app/job-details/[id].js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const JobDetails = () => {
+  return (
+   <Text>ssss</Text>
+  )
+}
+
+export default JobDetails
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -6,15 +6,15 @@ const useFetch = (endpoint, query) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // const options = {
-  //   method: "GET",
-  //   url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-  //   headers: {
-  //     "X-RapidAPI-Key": "6d6db642e6msh1b7b65f37f4d5e8p1d21cfjsnbd8f74bb387d",
-  //     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-  //   },
-  //   params: { ...query },
-  // };
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "6d6db642e6msh1b7b65f37f4d5e8p1d21cfjsnbd8f74bb387d",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
 
   const fetchData = async () => {
     setIsLoading(true);
